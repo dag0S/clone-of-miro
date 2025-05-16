711d5b44ad3476ca9b8253a735e5071f8c415050
Gosudarev Danila <danidagosudarev@gmail.com>
2025-05-16 21:51:23 +0300

    start project clone of miro

---
+++ src/app/router.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, redirect } from "react-router-dom";
+
+import { ROUTES } from "../shared/model/routes";
+import { App } from "./app";
+
+export const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: ROUTES.BOARDS,
+        lazy: () => import("@/features/boards-list/boards-list.page"),
+      },
+      {
+        path: ROUTES.BOARD,
+        lazy: () => import("@/features/board/board.page"),
+      },
+      {
+        path: ROUTES.LOGIN,
+        lazy: () => import("@/features/auth/login.page"),
+      },
+      {
+        path: ROUTES.REGISTER,
+        lazy: () => import("@/features/auth/register.page"),
+      },
+      {
+        path: ROUTES.HOME,
+        loader: () => redirect(ROUTES.BOARDS),
+      },
+    ],
+  },
+]);
---
+++ src/features/boards-list/boards-list.page.tsx
@@ -0,0 +1,15 @@
+import { href, Link } from "react-router-dom";
+
+import { ROUTES } from "@/shared/model/routes";
+
+function BoardsListPage() {
+  return (
+    <div>
+      <h1>Boards list</h1>
+
+      <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
+    </div>
+  );
+}
+
+export const Component = BoardsListPage;
